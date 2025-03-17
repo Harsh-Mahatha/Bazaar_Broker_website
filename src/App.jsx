3ea3d9ed95f500e06ce58b0084378107a34580df
Harsh-Mahatha <harshmahatha1847@gmail.com>
2025-03-18 01:49:13 +0530

    Cards Tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import Navigation from './components/Navigation';
-import BattlePage from './pages/BattlePage';
-import FAQPage from './pages/FAQPage';
+import { useState } from "react";
+import Navigation from "./components/Navigation";
+import BattlePage from "./pages/BattlePage";
+import FAQPage from "./pages/FAQPage";
 import Background from "./assets/Images/BG.png";
 import Logo from "./assets/Images/Logo.png";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('battle');
+  const [currentPage, setCurrentPage] = useState("battle");
 
   return (
     <div
@@ -14,17 +14,17 @@ export default function App() {
       style={{ backgroundImage: `url(${Background})` }}
     >
       <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      
-      {currentPage === 'battle' ? (
+
+      {currentPage === "battle" ? (
         <BattlePage />
-      ) : currentPage === 'faq' ? (
+      ) : currentPage === "faq" ? (
         <FAQPage />
       ) : null}
 
       <img
         src={Logo}
         alt="Logo"
-        className="fixed bottom-0 right-0 w-48 h-48 object-contain z-50 drop-shadow-2xl"
+        className="fixed bottom-0 right-4 w-80 h-80 object-contain z-50 drop-shadow-2xl mb-[-90px]"
       />
     </div>
   );
